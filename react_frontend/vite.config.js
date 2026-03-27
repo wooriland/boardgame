583e0853d341f1bb018fd5498f52a8af016294fc
wooriland <khcwang6@gmail.com>
2026-03-28 00:38:04 +0900

    Delete react_frontend directory

--- react_frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig(({ command }) => ({
-  plugins: [react()],
-  // ✅ dev: /
-  // ✅ build(GitHub Pages): /boardgame/
-  base: command === "serve" ? "/" : "/boardgame/",
-}));
